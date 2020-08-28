d8be398365db4adb9977310c14afef072ff0fa13
bakytkhamraev <khamraevbakyt@gmail.com>
2020-08-28 14:21:35 +0600

    design+api+glide+spinner

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.weatherapp;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,13 +10,17 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.weatherapp.data.Book;
 import com.example.weatherapp.data.RecyclerViewAdapter;
 import com.example.weatherapp.data.internet.RetrofitBuilder;
 import com.example.weatherapp.data.pojo.CurrentWeather;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -29,34 +34,49 @@ public class MainActivity extends AppCompatActivity {
     private TextView minTemp;
     private TextView maxTemp;
     private TextView sunrise;
+    private TextView daytime;
     private TextView sunset;
+    private TextView day;
+    private ImageView imageView;
+    private TextView sunny;
     private TextView wind;
-    List<Book> lstBook;
+    private List<Book> lstBook;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initView();
+        loadWeather();
+        listBook();
+        rv();
+    }
+
+    private void initView() {
         tvCurrentWeather = findViewById(R.id.tvCurrentWeather);
         humi = findViewById(R.id.humi);
         pressure = findViewById(R.id.pressure);
         minTemp = findViewById(R.id.min_temp);
         maxTemp = findViewById(R.id.max_temp);
+        imageView = findViewById(R.id.weather_img);
         sunrise = findViewById(R.id.sunrise);
         sunset = findViewById(R.id.sunset);
+        sunny = findViewById(R.id.sunny);
+        daytime = findViewById(R.id.daytime);
         wind = findViewById(R.id.wind_id);
-        loadWeather();
+        day = findViewById(R.id.day);
+    }
 
+    private void listBook() {
         lstBook = new ArrayList<>();
         lstBook.add(new Book("Mon, 21", "35°C  26°C", R.drawable.sunrise));
         lstBook.add(new Book("Tue, 22", "35°C  26°C", R.drawable.sunset));
-        lstBook.add(new Book("Wed, 22", "35°C  26°C", R.drawable.wind));
-        lstBook.add(new Book("Wed, 22", "35°C  26°C", R.drawable.sunny));
-        lstBook.add(new Book("Wed, 22", "35°C  26°C", R.drawable.sunny));
-        lstBook.add(new Book("Wed, 22", "35°C  26°C", R.drawable.sunny));
-        lstBook.add(new Book("Wed, 22", "35°C  26°C", R.drawable.sunny));
-        rv();
+        lstBook.add(new Book("Mon, 23", "35°C  26°C", R.drawable.barometer));
+        lstBook.add(new Book("Tue, 24", "35°C  26°C", R.drawable.humidity));
+        lstBook.add(new Book("Tue, 25", "35°C  26°C", R.drawable.wind));
+        lstBook.add(new Book("Mon, 26", "35°C  26°C", R.drawable.sunny));
+        lstBook.add(new Book("Mon, 27", "35°C  26°C", R.drawable.clock));
     }
 
     private void rv() {
@@ -68,7 +88,6 @@ public class MainActivity extends AppCompatActivity {
         myRV.setAdapter(myAdapter);
     }
 
-
     private void loadWeather() {
         RetrofitBuilder.getService().getCurrentWeather("Bishkek",
                 "4bbf5a1ed98cd9f688ebb3651474cdd2")
@@ -77,14 +96,39 @@ public class MainActivity extends AppCompatActivity {
                     public void onResponse(Call<CurrentWeather> call, Response<CurrentWeather> response) {
                         if (response.isSuccessful() && response.body() != null) {
                             CurrentWeather weather = response.body();
-                            tvCurrentWeather.setText(weather.getMain().getTemp().toString() + "C");
+                            sunny.setText(weather.getWeather().get(0).getMain());
+                            //иконка погоды
+                            Glide.with((MainActivity.this)).load("http://openweathermap.org/img/wn/" + weather.
+                                    getWeather().get(0).getIcon() + "@2x.png").centerCrop().into(imageView);
+                            tvCurrentWeather.setText(weather.getMain().getTempMin().toString() + "C");
                             humi.setText(weather.getMain().getHumidity().toString() + "%");
                             pressure.setText(weather.getMain().getPressure().toString() + "mBar");
                             maxTemp.setText(weather.getMain().getTempMax().toString() + "C");
                             minTemp.setText(weather.getMain().getTempMin().toString() + "C");
-                            sunrise.setText(weather.getSys().getSunrise().toString() + "AM");
-                            sunset.setText(weather.getSys().getSunset().toString() + "PM");
                             wind.setText(weather.getWind().getDeg().toString() + "km/h");
+
+                            //day
+                            String currentDate = new SimpleDateFormat("EEEE-dd-MM-yyyy-HH:mm", Locale.getDefault()).format(new Date());
+                            day.setText(currentDate);
+
+                            //sunrise
+                            long am = Long.valueOf(weather.getSys().getSunrise()) * 1000;
+                            Date sunrise1 = new java.util.Date(am);
+                            String sunr = new SimpleDateFormat(" hh:mma").format(sunrise1);
+                            sunrise.setText(sunr);
+
+                            //sunset
+                            long pm = Long.valueOf(weather.getSys().getSunset()) * 1000;
+                            Date sunset1 = new java.util.Date(pm);
+                            String suns = new SimpleDateFormat(" hh:mma").format(sunset1);
+                            sunset.setText(suns);
+
+                            //daytime
+                            int dayTimeMin = weather.getSys().getSunset() - weather.getSys().getSunrise();
+                            long pmam = dayTimeMin * 1000;
+                            Date dayTimeInNormal = new java.util.Date(pmam);
+                            String dayT = new SimpleDateFormat(" h:mma").format(dayTimeInNormal);
+                            daytime.setText(dayT);
 
                         }
                     }
--- app/src/main/java/com/example/weatherapp/data/internet/RetrofitService.java
+++ app/src/main/java/com/example/weatherapp/data/internet/RetrofitService.java
@@ -8,7 +8,6 @@ import retrofit2.http.Query;
 
 public interface RetrofitService {
 
-
     @GET("data/2.5/weather")
     Call<CurrentWeather>getCurrentWeather(@Query("q") String city, @Query("appid") String appId);
 
